418992c3a114cc5c6a45e639e6f053d9b2ae898b
grishma-kadam <grishma_kadam@persistent.com>
2023-01-11 14:46:21 +0530

    Frontend project created

---
+++ Recipe_Management_System/Recipe_Management_Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Recipe_Management_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
