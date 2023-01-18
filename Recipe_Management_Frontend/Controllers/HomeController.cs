146715e44d732b1a31cb685787faf2e6e6446792
grishma-kadam <grishma_kadam@persistent.com>
2023-01-18 12:38:33 +0530

    recipe id added

--- Recipe_Management_Frontend/Controllers/HomeController.cs
+++ Recipe_Management_Frontend/Controllers/HomeController.cs
@@ -145,8 +145,8 @@ namespace Recipe_Management_Frontend.Controllers
             return RedirectToAction("LogOut","Auth");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> EditRecipe(string recipeName, string category, string ingredientsList, string cookingProcess)
+
+        public async Task<IActionResult> EditRecipe(string recipeName, string category, string ingredientsList, string cookingProcess,int id)
         {
             try
             {
@@ -157,7 +157,7 @@ namespace Recipe_Management_Frontend.Controllers
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
                 HttpContent body = new StringContent(JsonConvert.SerializeObject(new { name = recipeName, ingredients = ingredientsList, procedure = cookingProcess, userId = userId, category = category}), System.Text.Encoding.UTF8, "application/json");
 
-                var response = client.PutAsync("Recipe/UpdateRecipe", body).Result;
+                var response = client.PutAsync("Recipe/UpdateRecipe?id="+id, body).Result;
 
                 var content = await response.Content.ReadAsStringAsync();
                 if (response.IsSuccessStatusCode)
